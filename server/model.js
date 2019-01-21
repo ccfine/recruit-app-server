ead35954758afe847be831df5b6f1e733eea2f12
卡卡罗特好可爱 <35161118+ccfine@users.noreply.github.com>
2019-01-21 22:41:04 +0800

    Update model.js

--- server/model.js
+++ server/model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MongoDB_URl = "mongodb://localhost:27017/recruit";
+const MongoDB_URl = "mongodb://127.0.0.1:27017/recruit";
 mongoose.connect(MongoDB_URl);
 
 const collections = {
